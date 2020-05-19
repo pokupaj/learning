723e01f67a06afd9daf9eef79458b51d17809856
pokupaj <pokupaj@gmail.com>
2020-05-19 14:48:52 +0700

    8.5 fixed

--- 8/8.5.js
+++ 8/8.5.js
@@ -3,8 +3,20 @@ const readlineSync = require("readline-sync");
 let str = readlineSync.question("Введите строку\n >");
 let indexP = readlineSync.question("Введите c какого индекса будем вырезать строку\n >");
 let lengthStrNew = readlineSync.question("Введите длину выреза\n >");
+let strArr = str.split("");
+let strNew = [];
 let p = parseInt(indexP);
 let l = parseInt(lengthStrNew);
 
 
-console.log(str.replace(str.slice(p, p+l), ""));
+//console.log(str.replace(str.slice(p, p+l), ""));
+
+for (let i=0; i<p; ++i) {
+    strNew.push(strArr[i]);
+}
+
+for (let i=p+l; i<strArr.length; ++i) {
+    strNew.push(strArr[i]);
+}
+
+console.log(strNew.join(""));
